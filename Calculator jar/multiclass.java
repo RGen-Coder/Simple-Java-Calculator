973bb72cf4afde8bfd2162bd42b8f63328ea796f
GenZone <92025739+RGen-Coder@users.noreply.github.com>
2021-10-06 21:16:58 +0800

    Add files via upload

---
+++ Calculator jar/multiclass.java
@@ -0,0 +1,34 @@
+package objectclass;
+import java.util.Scanner;
+public class multiclass {
+	
+	
+	public void multiplyclass()
+	{
+		Scanner scan = new Scanner(System.in);
+		try {
+			Thread.sleep(100);
+			System.out.println(".");
+			Thread.sleep(200);
+			System.out.println("..");
+			Thread.sleep(300);
+			System.out.println("...");
+			Thread.sleep(400);
+			System.out.println("....");
+			Thread.sleep(500);
+			System.out.println(".....");
+			Thread.sleep(600);
+			System.out.println("Addition Set");
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+		System.out.println("Enter Number A");
+		double a = scan.nextDouble();
+		System.out.println("Enter Number B");
+		double b = scan.nextDouble();
+		double c = a * b;
+		System.out.println("The Difference is " +c);
+	}
+
+}
